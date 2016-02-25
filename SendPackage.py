648176e6800da561208492e94629e642e491c88c
quangNguyen122 <bme.nnquang@gmail.com>
2016-02-25 08:09:46 +0700

    Increase baudrate
    from 9600 to 19200, to match hardware configuration,

--- SendPackage.py
+++ SendPackage.py
@@ -25,7 +25,7 @@ class SendPackage:
     def on_mainWindow_destroy(self,widget):
         if hasattr(self, 'ser'):
             if self.ser.isOpen():
-                self.ser.write(chr(120)) #Make sure the wc stops before closing the port
+                self.ser.write(chr(120))    # Make sure the wc stops before closing the port
                 self.ser.close()
         else:
             print "Serial port has never been created. Terminate the program."
@@ -36,28 +36,25 @@ class SendPackage:
         voltB = float(self.mych2.get_text())
         ao = int((voltA-1)*80 + 0.5)
         bo = int((voltB-1)*80 + 0.5)
-        # self.ser.write(chr(234))
-        # self.ser.write(chr(ao))
+        self.ser.write(chr(234))
+        self.ser.write(chr(ao))
         self.ser.write(chr(bo))
-        # Another way
-        # self.ser.write(234)
-        # self.ser.write(ao)
-        # self.ser.write(bo)
-        print "VoltA = %.2f     VoltB = %.2f" %(voltA, voltB)
-        print "ao = %d          bo = %d" %(ao, bo)
+
+        print "VoltA = %.2f     VoltB = %.2f" % (voltA, voltB)
+        print "ao = %d          bo = %d" % (ao, bo)
 
     def on_btnConnect_clicked(self,widget):
         self.Port = self.myPort.get_text()
         self.ser = serial.Serial(port="COM"+self.Port,
-                    baudrate=9600,
-                    bytesize=serial.EIGHTBITS,
-                    parity=serial.PARITY_NONE,
-                    stopbits=serial.STOPBITS_ONE,
-                    timeout=1)
+                                 baudrate=19200,
+                                 bytesize=serial.EIGHTBITS,
+                                 parity=serial.PARITY_NONE,
+                                 stopbits=serial.STOPBITS_ONE,
+                                 timeout=1)
         print(self.ser.name)
-        time.sleep(2)   # Give time for Arduino to prepare
-        self.ser.flush()
-        hello = self.ser.read(100)
+        time.sleep(2)                   # Give time for microcontroller to prepare
+        self.ser.flush()                # flush all data from memory
+        hello = self.ser.read(100)      # read all data from memory (if any)
         print hello
         self.myStatus.set_text("CONNECTED")
 
