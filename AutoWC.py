b9339d0837e1b8f22e4a236c68ab04ef1b2354b3
quangNguyen122 <bme.nnquang@gmail.com>
2016-03-02 01:25:32 +0700

    Add compute distance

--- AutoWC.py
+++ AutoWC.py
@@ -4,7 +4,9 @@
 Main program
 """
 
+import numpy as np
 from GetDataSerial import Serial
+from WheelchairModel import WheelchairModel
 
 class AutoWC:
 
@@ -14,8 +16,13 @@ class AutoWC:
         self.arrayRightTicks = None
         self.sumRightTicks = None
 
+        # Set up port for incoming data
         self.serial = Serial()
         self.port = self.serial.ser
+
+        # Wheelchair model
+        self.wc = WheelchairModel()
+
 
     def sendPackage(self, channel, volt):
         i = int((volt-1)*80 + 0.5)
@@ -36,16 +43,28 @@ class AutoWC:
         self.sendPackage('a', 2.5)
         self.sendPackage('b', 2.5)
 
-    def updateTicks(self, buffer):
+    def updateTicks(self):
+        """Update  arrayTicks to print sumTicks to calculate distance
+        :returns d_left: distance travelled from left wheel
+                 d_right: distance travelled from right wheel
         """
-        update  arrayTicks to print
-                sumTicks to calculate distance
-        :param buffer: buffer from serial port
-        """
-        for x in range (0, len(buffer)):
-            if x % 2 == 0:
-                self.arrayLeftTicks[x] = str(ord(buffer[x]))
-                self.sumLeftTicks += ord(buffer[x])
-            else:
-                self.arrayRightTicks[x] = str(ord(buffer[x]))
-                self.sumRightTicks += ord(buffer[x])
+        previous_d_right = self.wc.d_right
+        previous_d_left = self.wc.d_left
+
+        if self.port.inWaiting() > 0:
+            buffer = Serial.readPort()  # numbers of ticks of 2 encoders
+
+            LEFT_M_PER_TICK = 2*np.pi*self.wc.R_left/self.wc.encoder_revolution
+            RIGHT_M_PER_TICK = 2*np.pi*self.wc.R_right/self.wc.encoder_revolution
+            for x in range(0, len(buffer)):
+                if x % 2 == 0:      # left encoders
+                    self.arrayLeftTicks[x] = str(ord(buffer[x]))
+                    # self.sumLeftTicks += ord(buffer[x])
+                    self.wc.d_left += ord(buffer[x])*LEFT_M_PER_TICK
+                else:               # right encoders
+                    self.arrayRightTicks[x] = str(ord(buffer[x]))
+                    # self.sumRightTicks += ord(buffer[x])
+                    self.wc.d_right += ord(buffer[x])*RIGHT_M_PER_TICK
+
+        print 'd_left: %.2f\td_right: %.2f' % (self.wc.d_left, self.wc.d_right)
+        return self.wc.d_left, self.wc.d_right
--- WheelchairModel.py
+++ WheelchairModel.py
@@ -1,21 +1,29 @@
 class WheelchairModel:
+    """ Model of the wheel chair. Store the description of WC at any given time
+
+    Attributes:
+        wc_wheel_base, wc_left_base, wc_right_radius, wc_encoder_revolution (float): constant
+        wc_x, wc_y, wc_theta (float): current position
+        wc_left_travel_distance: travelled distance
+        wc_vel_distance: velocity
+    """
     def __init__(self):
-        self.wc_wheel_base = 0.52
-        self.wc_left_base = 0.165
-        self.wc_right_radius = 0.165
-        self.wc_encoder_revolution = 3200
+        self.wheel_base = 0.52
+        self.R_left = 0.165
+        self.R_right = 0.165
+        self.encoder_revolution = 3200
 
         # wc position
-        self.wc_x = 0
-        self.wc_y = 0
-        self.wc_theta = 0
+        self.x = 0
+        self.y = 0
+        self.theta = 0
 
         # wc travel distance
-        self.wc_left_travel_distance = 0
-        self.wc_right_travel_distance = 0
+        self.d_left = 0
+        self.d_right= 0
 
         # wheel velocity
-        self.wc_vel_distance_left = 0
-        self.wc_vel_distance_right = 0
-        self.wc_vel_dt = 0
-        
+        self.vel_left = 0
+        self.vel_right = 0
+        self.vel_dt = 0
+
